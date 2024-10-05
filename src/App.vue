9412663e9a7a6d96551083d3321b976279870132
筑乐 <2479003541@qq.com>
2024-10-05 19:02:28 +0800

    真正初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {ref} from  'vue'
+const msg = ref<String>();
+const clik = () => {
+  msg.value = "Hellow,world"
+}
+</script>
+
+<template>
+<h1>
+  {{ msg }}
+</h1>
+<button @click="clik">世界你好</button>
+</template>
+
+<style scoped>
+
+</style>
